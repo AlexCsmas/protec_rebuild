bfaf6258c84b810a21cec790620a867fd641e705
AlexCsmas <2alexcosmas@gmail.com>
2019-09-18 14:33:00 +0300

    test-contact

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -10,6 +10,7 @@ const _21593cf0 = () => interopDefault(import('..\\pages\\courses\\index.vue' /*
 const _a1b22142 = () => interopDefault(import('..\\pages\\default\\index.vue' /* webpackChunkName: "pages_default_index" */))
 const _383b3056 = () => interopDefault(import('..\\pages\\latest_news\\index.vue' /* webpackChunkName: "pages_latest_news_index" */))
 const _e7a34d94 = () => interopDefault(import('..\\pages\\services\\index.vue' /* webpackChunkName: "pages_services_index" */))
+const _583d3c98 = () => interopDefault(import('..\\pages\\contact\\test.vue' /* webpackChunkName: "pages_contact_test" */))
 const _eb76a99a = () => interopDefault(import('..\\pages\\course\\safety_management_course.vue' /* webpackChunkName: "pages_course_safety_management_course" */))
 const _4b683c51 = () => interopDefault(import('..\\pages\\courses\\coursecat\\index.vue' /* webpackChunkName: "pages_courses_coursecat_index" */))
 const _ed2a2588 = () => interopDefault(import('..\\pages\\courses\\safety_management\\index.vue' /* webpackChunkName: "pages_courses_safety_management_index" */))
@@ -164,6 +165,10 @@ export function createRouter() {
       path: "/services",
       component: _e7a34d94,
       name: "services"
+    }, {
+      path: "/contact/test",
+      component: _583d3c98,
+      name: "contact-test"
     }, {
       path: "/course/safety_management_course",
       component: _eb76a99a,
---
+++ pages/contact/test.vue
@@ -0,0 +1,102 @@
+<template>
+
+
+    <section>
+       <div class="w-full bg-white">
+
+		    <!-- title -->
+			<div class="text-center px-6 py-12 bg-gray-100 border-b">
+				<h1 class="font-display-italic font-extrabold text-5xl md:text-5xl leading-none py-4">Contact Us</h1>
+				<p class="leading-loose text-gray-dark">
+					Let have a conversation. 
+				</p>
+			</div>
+			<!-- /title -->
+
+            <div class=" mx-auto lg:px-16">
+
+                <div class=" md:flex flex-row ">
+
+                    <div class="w-full md:w-1/3 ">
+                    <div class=" bg-primary p-8 text-white">
+                        <p class="text-white font-bold mb-1">
+							Protec Consulting East Africa Limited
+						</p>
+
+                        <div class=" py-4 ">
+
+                             <p class="text-white font-bold mb-1">
+							Location
+                            </p>
+                            <p class="text-sm mb-2">
+                                Westlands Road,<br>							
+                                Madonna House, ,<br>	3rd Floor, Room 314<br>
+                                Westlands
+                            </p>
+
+                        </div>
+
+                        <div>
+                            <p class="text-white font-bold mb-1">
+                                Contacts
+                            </p>
+                            <p class="text-sm mb-2">
+                                E: info.ea@weareprotecafrica.com<br>							
+                                M: +254 715 742 898<br>	
+             
+                            </p>
+                        </div>
+                       
+                    </div>
+
+
+                </div> <!-- End of Column --> 
+                
+                <div class="w-full md: w-2/3" >
+                    <div class="bg-gray-200">
+
+                        <form name="contact" method="POST" data-netlify="true">
+                            <p>
+                                <label>Your Name: <input type="text" name="name" /></label>   
+                            </p>
+                            <p>
+                                <label>Your Email: <input type="email" name="email" /></label>
+                            </p>
+                            <p>
+                                <label>Your Role: <select name="role[]" multiple>
+                                <option value="leader">Leader</option>
+                                <option value="follower">Follower</option>
+                                </select></label>
+                            </p>
+                            <p>
+                                <label>Message: <textarea name="message"></textarea></label>
+                            </p>
+                            <p>
+                                <button type="submit">Send</button>
+                            </p>
+                        </form>
+
+                                 
+                    </div>
+                </div>   <!-- End of Column --> 
+
+                </div> <!-- End of Row -->
+
+                      
+            </div>
+
+
+		</div>
+    </section>
+</template>
+
+<script>
+import Posts from '~/components/Posts.vue'
+
+export default {
+  components: {
+    Posts
+  },
+  // ...
+}
+</script>
