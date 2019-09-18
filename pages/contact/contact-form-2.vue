a5a8b7f51eec7ae445f8da6d21433b3a00a32004
AlexCsmas <2alexcosmas@gmail.com>
2019-09-18 17:17:53 +0300

    Nuxt.js netlify form-handling

--- pages/contact/contact-form-2.vue
+++ pages/contact/contact-form-2.vue
@@ -1,17 +1,7 @@
 <template>
-    <form name="contact-form-template" action="" method="post" netlify>
-        <label class="form-label" for="name">
-          Name:
-        </label>
-        <input class="form-field" name="name" id="name" />
-        <label class="form-label" for="email">
-          Email:
-        </label>
-        <input class="form-field" name="email" id="email" />
-        <label class="form-label" for="message">
-          Message:
-        </label>
-        <textarea class="form-field" name="message" id="message"></textarea>
-        <input class="form-button" type="submit" value="Send message" />
-      </form>
+   <form name="contact-form" netlify netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
+</form>
 </template>
